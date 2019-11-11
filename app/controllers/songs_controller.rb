5652dd111c75c33be2f794cdf542c118e82f8dbd
jamieberrier <berrier.jamie@gmail.com>
2019-11-11 23:43:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     @song.update(song_params(:name, :artist_id, :genre_id))
-    redirect_to song_path(@somg)
+    redirect_to song_path(@song)
   end
 
   private
