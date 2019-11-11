fcf9445c3a78ed1991eb23288807f1f8832db4bc
jamieberrier <berrier.jamie@gmail.com>
2019-11-11 23:20:44 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -24,7 +24,7 @@ class GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params(:name))
-    redirect_to artist_path(@genre)
+    redirect_to genre_path(@genre)
   end
 
   private
